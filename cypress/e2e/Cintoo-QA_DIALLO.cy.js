d287f2957332a6c86a9f4835bbab8dd38fcb566c
abdel-dialo <diallo.abdoulgadirou@gmail.com>
2024-08-22 16:28:38 +0200

    add page object model

--- cypress/e2e/Cintoo-QA_DIALLO.cy.js
+++ cypress/e2e/Cintoo-QA_DIALLO.cy.js
@@ -1,34 +1,34 @@
+import showcasePage from  '../support/PageObjects/showcasePage.js';
+
 describe('Cintoo Showcase page', () => {
 
    const numberDemoProject = 4
    const textDemoProjet = 'Play 3D Demo'
+   const showcasePageObjet = new showcasePage()
+
 
   beforeEach(() => {
     cy.visit(Cypress.env('showcase_url')) 
   })
 
   it('Check the number of demo projects on the showcase page', () => { 
-    cy.get('.lqd-fb-content')
+    showcasePageObjet.elements.projetListDemo()
     .should('have.length', numberDemoProject)
-    .find('span[data-text="Play 3D Demo"]')
-      .should(($span) => {
-         expect($span).to.contain(textDemoProjet)
       })  
-  })
+
 
   it('Check that each project demo contains an image and a button', () => {
-    cy.get('.lqd-fb-content')
+    showcasePageObjet.elements.projetListDemo()
     .each(($li, index, $lis) => {
       // Check  each project demo a button  with the text: "Play 3D Demo"
-      cy.get('a.elementor-button>span[data-text="Play 3D Demo"]').eq(index).contains(textDemoProjet)
+      showcasePageObjet.elements.projetDemoButton().eq(index).contains(textDemoProjet)
       // Check that each project demo contains an image and a button
-      cy.get('div.lqd-fb-img').eq(index).find('img').should('be.visible')
+      showcasePageObjet.elements.projetDemoImage().eq(index).find('img').should('be.visible')
     })
   })
 
   it('Check Buy URL contains /buy', () => {
-    cy.get('a[href="/buy"]').contains('span','buy now').click()
-    cy.wait(8000)
-    cy.url().should('include', '/buy/')  
+    showcasePageObjet.elements.buyButton().click()
+    cy.url({ timeout: 30000 }).should('include', '/buy/')  
   })
 })
